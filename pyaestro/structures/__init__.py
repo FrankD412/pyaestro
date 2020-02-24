4828a802919b64b66b222b9193c3c81ee31d6efa
Frank Di Natale <frank.dinatale1988@gmail.com>
2020-02-23 19:03:45 -0800

    Addition of abstract graph.

--- pyaestro/structures/__init__.py
+++ pyaestro/structures/__init__.py
@@ -40,21 +40,6 @@ class Graph(ABC):
         pass
 
 
-class MultiGraph(Graph):
-    def __init__(self):
-        self._vertices = {}
-        self._edges = defaultdict(defaultdict(list))
-        self._layers = {}
-
-    def add_layer(self, layer, layer_type):
-        self._layers[layer] = layer_type
-
-    def add_vertex(self, vertex, value=None, *, layer="__default__"):
-        self._vertices[layer][vertex].append(value)
-
-    def add_edge(self, a, b, layer="__default__"):
-        layer_type = self._layers[layer]
-
 
 
     
