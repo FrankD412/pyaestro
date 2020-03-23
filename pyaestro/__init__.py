dbf6258d7fcd1771ce5fec82b84169038306a904
Frank Di Natale <frank.dinatale1988@gmail.com>
2020-03-22 22:47:49 -0700

    Addition of version.

--- pyaestro/__init__.py
+++ pyaestro/__init__.py
@@ -4,3 +4,6 @@ The pyaestro module is a collection of general workflow components.
 structures - data structures for managing workflows.
 
 """
+
+__version_info__ = ("0", "0", "1a")
+__version__ = '.'.join(__version_info__)
