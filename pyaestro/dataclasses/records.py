b7e13c8dab2449fbd9aaa55ce5634057b5bddf45
Frank Di Natale <frank.dinatale1988@gmail.com>
2020-03-23 22:32:15 -0700

    Addition of the StepRecord dataclass.

--- pyaestro/dataclasses/records.py
+++ pyaestro/dataclasses/records.py
@@ -1,6 +1,24 @@
+"""A module dedicated to data records."""
 from dataclasses import dataclass
 from datetime import datetime
+from collections import defaultdict
 from enum import Enum
+
+
+class StepState(Enum):
+    """States that a workflow step can reside in."""
+    INITIALIZED = 0
+    QUEUED = 1
+    WAITING = 2
+    RUNNING = 3
+    FINISHING = 4
+    FINISHED = 5
+    FAILED = 6
+    INCOMPLETE = 7
+    HWFAILURE = 8
+    TIMEDOUT = 9
+    UNKNOWN = 10
+    CANCELLED = 11
 
 
 @dataclass
@@ -10,5 +28,19 @@ class StepRecord:
     base_name: str
     combination: str
 
-    time_ledger: dict
-    cur_state: StateEnum
+    time_ledger: defaultdict(datetime.now)
+    cur_state: StepState
+
+    def mark_state(self, state):
+        """Mark the state of the step and the time it was marked."""
+        if not isinstance(state, StepState):
+            raise TypeError("Expected a state of type StepState.")
+
+        self.time_ledger[state]
+        self.cur_state = state
+
+    def time_diff(self, state_a, state_b):
+        pass
+
+    def write_script(self, path, adapter):
+        pass
