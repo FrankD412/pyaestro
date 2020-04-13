b47d2d80509c535c3319b80ff660f61aa665c387
Frank Di Natale <frank.dinatale1988@gmail.com>
2020-04-12 22:46:25 -0700

    Removal of Singleton from structures.

--- pyaestro/structures/__init__.py
+++ pyaestro/structures/__init__.py
@@ -1,18 +1,2 @@
 """A module of different graph types and other properties."""
 
-__all__ = "Singleton"
-
-class Singleton(type):
-    """
-    A Singleton metaclass for implementing the singleton design pattern.
-
-    Implementation pulled from StackOverflow
-    https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
-    """
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = \
-                super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
