03de0f0cb5f5b82a3d95d4cd604bd2d3102370d4
Frank Di Natale <frank.dinatale1988@gmail.com>
2020-04-12 22:45:58 -0700

    Addition of a Singleton Metaclass.

--- pyaestro/abstracts/__init__.py
+++ pyaestro/abstracts/__init__.py
@@ -1,1 +1,18 @@
 """A module containing a set of base abstract classes."""
+
+__all__ = "Singleton"
+
+class Singleton(type):
+    """
+    A Singleton metaclass for implementing the singleton design pattern.
+
+    Implementation pulled from StackOverflow
+    https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
+    """
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = \
+                super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
