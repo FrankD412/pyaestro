48d089c8d02d71b5e99e13ad02c2fe3f59116d2a
Frank Di Natale <frank.dinatale1988@gmail.com>
2020-04-13 22:48:56 -0700

    Multireader 1 writer dict.

--- pyaestro/structures/__init__.py
+++ pyaestro/structures/__init__.py
@@ -1,2 +1,31 @@
-"""A module of different graph types and other properties."""
+"""A module of data structures and other class important to workflow."""
+from threading import Condition, Lock
 
+
+class MultiRdr1WrDict(dict):
+    def __init__(self, *args, **kwargs):
+        self._g_lock = Condition(Lock())
+        self._readers = 0
+
+    def __getitem__(self, key):
+        self._g_lock.acquire()
+        self._readers += 1
+        self._g_lock.release()
+
+        item = super().__getitem__(key)
+
+        self._g_lock.acquire()
+        self._readers -= 1
+        if self._readers == 0:
+            self._g_lock.notify()
+        self._g_lock.release()
+
+        return item
+
+    def __setitem__(self, key, value):
+        self._g_lock.acquire()
+        while self._readers > 0:
+            self._g_lock.wait()
+
+        super().__setitem__(key, value)
+        self._g_lock.release()
