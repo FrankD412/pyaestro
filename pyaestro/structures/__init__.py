057cc6dd8d5d424ec6e15e3b8b34b57af1ea428c
Frank Di Natale <frank.dinatale1988@gmail.com>
2020-04-16 23:19:40 -0700

    Correction to prevent starving writer.

--- pyaestro/structures/__init__.py
+++ pyaestro/structures/__init__.py
@@ -5,10 +5,14 @@ from threading import Condition, Lock
 class MultiRdr1WrDict(dict):
     def __init__(self, *args, **kwargs):
         self._g_lock = Condition(Lock())
+        self._w_wait = False
         self._readers = 0
 
     def __getitem__(self, key):
         self._g_lock.acquire()
+        while self._w_wait:
+            self._g_lock.wait()
+
         self._readers += 1
         self._g_lock.release()
 
@@ -24,8 +28,12 @@ class MultiRdr1WrDict(dict):
 
     def __setitem__(self, key, value):
         self._g_lock.acquire()
+        self._w_wait = True
         while self._readers > 0:
             self._g_lock.wait()
 
         super().__setitem__(key, value)
+
+        self._w_wait = False
+        self._g_lock.notifyAll()
         self._g_lock.release()
