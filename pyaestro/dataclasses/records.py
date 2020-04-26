7abe48f4d29a169e729d5dd45ef7d340fa2c4bb1
Frank Di Natale <frank.dinatale1988@gmail.com>
2020-04-25 23:06:55 -0700

    Move Singleton to metaclasses.

--- pyaestro/dataclasses/records.py
+++ pyaestro/dataclasses/records.py
@@ -5,22 +5,6 @@ from collections import defaultdict
 from enum import Enum
 
 
-class StepState(Enum):
-    """States that a workflow step can reside in."""
-    INITIALIZED = 0
-    QUEUED = 1
-    WAITING = 2
-    RUNNING = 3
-    FINISHING = 4
-    FINISHED = 5
-    FAILED = 6
-    INCOMPLETE = 7
-    HWFAILURE = 8
-    TIMEDOUT = 9
-    UNKNOWN = 10
-    CANCELLED = 11
-
-
 @dataclass
 class StepRecord:
     """A class to track a tasks state and record timing information"""
