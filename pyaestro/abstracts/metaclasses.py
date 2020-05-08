5c5bd84662220ca6c8af2dc20f1a89a11f2c9566
Frank Di Natale <frank.dinatale1988@gmail.com>
2020-05-07 19:49:04 -0700

    Removal of print statements.

--- pyaestro/abstracts/metaclasses.py
+++ pyaestro/abstracts/metaclasses.py
@@ -27,15 +27,14 @@ class SynchronizedClass(type):
         lock = RLock()
 
         _init = sync_cls.__init__
+
         def __init__(self, *args, **kwargs):
             self.__lock__ = lock
             _init(self, *args, **kwargs)
 
         sync_cls.__init__ = __init__
 
-        print(inspect.getmembers(sync_cls, inspect.isroutine))
         for method in inspect.getmembers(sync_cls, inspect.isroutine):
-            print("Syncing -- ", method[0])
             sync = cls.synchronize(lock)
             setattr(
                 sync_cls, method[0],
