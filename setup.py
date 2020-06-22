89dacb3d129ca2724ec06862bd6adfe0c7b9956f
Frank Di Natale <frank.dinatale1988@gmail.com>
2020-06-21 19:38:06 -0700

    Updates to pyaestro setup.py.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
   name='pyaestro',
-  description='A tool to easily orchestrate general computational workflows '
-  'both locally and on supercomputers.',
+  description='A package of utilities structures for building workflows'
+  ' and workflow related tools.',
   version=__version__,
   author='Francesco Di Natale',
   maintainer='Francesco Di Natale',
   author_email='frank.dinatale1988@gmail.com',
-  url='https://github.com/llnl/maestrowf',
+  url='https://github.com/FrankD412/pyaestro',
   license='MIT License',
   packages=find_packages(),
   entry_points={
@@ -21,16 +21,19 @@ setup(
   download_url='https://pypi.org/project/pyaestro/',
   python_requires='>3.4.*',
   classifiers=[
-    'Development Status :: 5 - Production/Stable',
+    'Development Status :: 1 - Planning',
     'Operating System :: Unix',
     'Operating System :: MacOS :: MacOS X',
     'Intended Audience :: Developers',
     'Intended Audience :: Education',
     'Intended Audience :: Science/Research',
     'Topic :: Scientific/Engineering',
-    'Topic :: System :: Distributed Computing',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
   ],
+  project_urls={
+    'Source': 'https://github.com/FrankD412/pyaestro',
+    'Tracker': 'https://github.com/FrankD412/pyaestro/issues',
+  },
 )
