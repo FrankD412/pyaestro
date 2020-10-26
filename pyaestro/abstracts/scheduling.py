53cede428d86212cfaf3831669e293686897ea63
Francesco Di Natale <dinatale3@llnl.gov>
2020-10-26 12:56:13 -0400

    Shift of scheduling interfaces to mixins.

--- pyaestro/abstracts/scheduling.py
+++ pyaestro/abstracts/scheduling.py
@@ -1,27 +1,35 @@
 """A module that defines interfaces for interacting with schedulers."""
-from abc import ABC, abstractmethod
+from abc import ABC, abstractclassmethod
 from enum import Enum
+from pathlib import Path
+from typing import Dict, List, Union
 
 
-class SchedulerAdapter(ABC):
+class TaskState(Enum):
+    """An enumeration for task state."""
+    BLOCKED = 0
+    PENDING = 1
+    WAITING = 2
+    RUNNING = 3
+    FINISHED = 4
+    FAILED = 5
+    INCOMPLETE = 6
+    HWFAILURE = 7
+    TIMEDOUT = 8
+    UNKNOWN = 9
+    CANCELLED = 10
+
+
+IDENTIFIER_HINT = Union[str, int]
+JOBLIST_HINT = Union[List[int], List[str]]
+STATUS_HINT = Union[Dict[int, TaskState], Dict[str, TaskState]]
+
+
+class SchedulerMixIn(ABC):
     """An interface for interacting with system schedulers."""
 
-    @abstractmethod
-    def __init__(self, **kwargs):
-        """
-        Create an instance to interface with a scheduler.
-
-        :params kwargs: Settings for this instance of adapter.
-        """
-
-    @abstractmethod
-    def get_header(self, shell, resources):
-        """
-        Generate the header for a scheduler script of this type.
-        """
-
-    @abstractmethod
-    def submit(self, script, workspace, **kwargs):
+    @abstractclassmethod
+    def submit(cls, command: str, workspace: Path, **kwargs):
         """
         Submit a script to the system scheduler for execution.
 
@@ -30,8 +38,8 @@ class SchedulerAdapter(ABC):
         :returns: A SubmissionRecord instance.
         """
 
-    @abstractmethod
-    def job_status(self, jobid):
+    @abstractclassmethod
+    def job_status(cls, identifier: IDENTIFIER_HINT) -> TaskState:
         """
         Check the status of a single job.
 
@@ -39,11 +47,21 @@ class SchedulerAdapter(ABC):
         :returns: An enumerated job status.
         """
 
-    @abstractmethod
-    def joblist_status(self, joblist):
+    @abstractclassmethod
+    def joblist_status(cls, joblist: JOBLIST_HINT) -> STATUS_HINT:
         """
         Check the status of a list of jobs.
 
         :params joblist: A list containing strings of job identifiers.
         :returns: A dictionary mapping jobid to job status.
+        """
+
+
+class SchedulerHeaderMixIn(ABC):
+    """PASS"""
+
+    @abstractclassmethod
+    def get_header(self, shell: Path, resources):
+        """
+        Generate the header for a scheduler script of this type.
         """
