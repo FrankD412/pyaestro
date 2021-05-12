0a6db619a567f9dfc354454fef722af2479b5726
Francesco Di Natale <frank.dinatale1988@gmail.com>
2021-05-11 23:36:14 -0700

    Initial pass of abstract testing.

---
+++ tests/core/datastructures/test_abstracts.py
@@ -0,0 +1,55 @@
+import pytest
+from typing import Hashable, Iterable, Tuple
+
+from pyaestro.core.datastructures.abstracts import Graph, AdjacencyGraph
+
+
+class ConcreteAbstractGraph(Graph):
+    def _delete_edges(self, key: Hashable) -> None:
+        pass
+
+    def edges(self) -> Iterable[Tuple[Hashable]]:
+        pass
+
+    def add_edge(self, a: Hashable, b: Hashable) -> None:
+        pass
+
+    def remove_edge(self, a: Hashable, b: Hashable) -> None:
+        pass
+    
+    def delete_edges(self, key: Hashable) -> None:
+        pass
+    
+    def get_neighbors(self, node: Hashable) -> Iterable[Hashable]:
+        raise StopIteration
+
+    def __iter__(self) -> Iterable[str]:
+        pass
+
+
+@pytest.fixture(scope="function")
+def concrete_graph():
+    return ConcreteAbstractGraph()
+
+
+class TestGraph:
+    def test_contains(concrete_graph):
+        pass
+    
+    def test_getitem(concrete_graph):
+        pass
+    
+    def test_setitem(concrete_graph):
+        pass
+    
+    def test_delitem(concrete_graph):
+        pass
+    
+    def test_repr(concrete_graph):
+        pass
+    
+    def test_dfs(concrete_graph):
+        pass
+    
+    def test_bfs(concrete_graph):
+        pass
