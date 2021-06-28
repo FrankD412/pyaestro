159cfd884c56d2f8fd1aabaf2f9f491b5b6c8251
Francesco Di Natale <frank.dinatale1988@gmail.com>
2021-06-28 09:21:12 -0700

    Move fixtures to central conftest.py

--- tests/core/datastructures/conftest.py
+++ tests/core/datastructures/conftest.py
@@ -43,27 +43,4 @@ def diamond_graph():
     }
 
 
-@pytest.fixture(scope="module",
-                params=[{"malformed": {}, "vertices": {}},
-                        {"edges": {}, "malformed": {}},
-                        {"malformed1": {}, "malformed2": {}}])
-def malformed_specification(request):
-    return request.param
 
-
-@pytest.fixture(scope="module",
-                params=["linear_graph", "diamond_graph"])
-def valid_specification(request):
-    return request.getfixturevalue(request.param)
-
-
-@pytest.fixture(scope="function",
-                params=[1, 2, 4, 7, 8, 16, 32])
-def sized_node_list(request):
-    node_list = []
-    for length in itertools.count(1):
-        for i in itertools.product(ascii_uppercase, repeat=length):
-            node_list.append("".join(i))
-
-        if len(node_list) >= request.param:
-            return node_list[:request.param]
