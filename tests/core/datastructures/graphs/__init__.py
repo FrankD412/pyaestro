abedce399a67dd5d3e2a9ec803a8dd043f451306
Francesco Di Natale <frank.dinatale1988@gmail.com>
2021-08-05 19:26:55 -0400

    Updates to testing to use Edge dataclass.

---
+++ tests/core/datastructures/graphs/__init__.py
@@ -0,0 +1,23 @@
+from typing import Hashable, Iterable, Tuple
+
+from pyaestro.core.datastructures.abstracts import Graph
+from pyaestro.typing import Comparable
+
+class ConcreteAbstractGraph(Graph):
+    def _delete_edges(self, key: Hashable) -> None:
+        pass
+
+    def edges(self) -> Iterable[Tuple[Hashable]]:
+        pass
+
+    def add_edge(self, a: Hashable, b: Hashable, weight: Comparable=0) -> None:
+        pass
+
+    def remove_edge(self, a: Hashable, b: Hashable) -> None:
+        pass
+
+    def delete_edges(self, key: Hashable) -> None:
+        pass
+
+    def get_neighbors(self, node: Hashable) -> Iterable[Hashable]:
+        raise StopIteration
