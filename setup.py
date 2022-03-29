7c6ced6f569c8285d427b0bc536b44a803685978
Francesco Di Natale <frank.dinatale1988@gmail.com>
2022-03-29 09:49:32 -0700

    Drop support for python < 3.7

--- setup.py
+++ setup.py
@@ -11,49 +11,48 @@ def load_readme():
     :returns: A string containing the contents of README.md.
     """
     pkg_path = path.abspath(path.dirname(__file__))
-    with open(path.join(pkg_path, 'README.md')) as f:
+    with open(path.join(pkg_path, "README.md")) as f:
         long_description = f.read()
 
     return long_description
 
 
 setup(
-  name='pyaestro',
-  description='A package of utilities structures for building workflows'
-  ' and workflow related tools.',
-  version=__version__,
-  author='Francesco Di Natale',
-  maintainer='Francesco Di Natale',
-  author_email='frank.dinatale1988@gmail.com',
-  url='https://github.com/FrankD412/pyaestro',
-  license='MIT License',
-  packages=find_packages(),
-  entry_points={
-    'console_scripts': []
-  },
-  install_requires=[
-    "psutil",
-    "coloredlogs",
-  ],
-  extras_require={},
-  long_description=load_readme(),
-  long_description_content_type='text/markdown',
-  download_url='https://pypi.org/project/pyaestro/',
-  python_requires='>3.4.*',
-  classifiers=[
-    'Development Status :: 1 - Planning',
-    'Operating System :: Unix',
-    'Operating System :: MacOS :: MacOS X',
-    'Intended Audience :: Developers',
-    'Intended Audience :: Education',
-    'Intended Audience :: Science/Research',
-    'Topic :: Scientific/Engineering',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-  ],
-  project_urls={
-    'Source': 'https://github.com/FrankD412/pyaestro',
-    'Tracker': 'https://github.com/FrankD412/pyaestro/issues',
-  },
+    name="pyaestro",
+    description="A package of utilities structures for building workflows"
+    " and workflow related tools.",
+    version=__version__,
+    author="Francesco Di Natale",
+    maintainer="Francesco Di Natale",
+    author_email="frank.dinatale1988@gmail.com",
+    url="https://github.com/FrankD412/pyaestro",
+    license="MIT License",
+    packages=find_packages(),
+    entry_points={"console_scripts": []},
+    install_requires=[
+        "psutil",
+        "coloredlogs",
+        "pydantic",
+    ],
+    extras_require={},
+    long_description=load_readme(),
+    long_description_content_type="text/markdown",
+    download_url="https://pypi.org/project/pyaestro/",
+    python_requires=">=3.7.*",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    project_urls={
+        "Source": "https://github.com/FrankD412/pyaestro",
+        "Tracker": "https://github.com/FrankD412/pyaestro/issues",
+    },
 )
