f478f3952edc770e80018c89394e6efb72d0eca3
Frank Di Natale <frank.dinatale1988@gmail.com>
2022-06-12 23:30:31 -0700

    Initial implementation of linear chain test.

--- tests/structures/graphs/test_algorithms.py
+++ tests/structures/graphs/test_algorithms.py
@@ -1,6 +1,7 @@
 import pytest
-from typing import List
+from typing import List, Type
 
+from pyaestro.abstracts.graphs import Graph
 from pyaestro.structures.graphs.algorithms import (
     BreadthFirstSearch,
     DepthFirstSearch,
@@ -16,19 +17,27 @@ GRAPHS = (AcyclicAdjGraph, AdjacencyGraph, BidirectionalAdjGraph)
 
 
 @pytest.mark.parametrize("algorithm", (BreadthFirstSearch, DepthFirstSearch))
+@pytest.mark.parametrize("graph_type", GRAPHS)
 class TestGraphAlgorithms:
     def test_tree_search(
-        self, sized_node_list: List[str], algorithm: GraphSearchProtocol
+        self, sized_node_list: List[str], algorithm: GraphSearchProtocol,
+        graph_type: Type[Graph]
     ) -> None:
         """Tests path searching on a tree-structured graph.
 
         Args:
             sized_node_list (List[str]): A list of unique node names.
             algorithm (GraphSearchProtocol): Search algorithm run testing on.
+            graph_type (Type[Graph]): A Graph class name to test.
         """
-        pass
+        g = graph_type()
+        for node in sized_node_list:
+            g[node] = None
 
-    def test_linear_search(self, sized_node_list: List[str], algorithm: GraphSearchProtocol) -> None:
+    def test_linear_search(
+        self, sized_node_list: List[str], algorithm: GraphSearchProtocol,
+        graph_type: Type[Graph]
+    ) -> None:
         """Tests path searching on a linear chain graph.
 
         Passing condition is that the search returns a path that matches the
@@ -38,5 +47,23 @@ class TestGraphAlgorithms:
         Args:
             sized_node_list (List[str]): A list of unique node names.
             algorithm (GraphSearchProtocol): Search algorithm run testing on.
+            graph_type (Type[Graph]): A Graph class name to test.
         """
-        pass
+        print(sized_node_list)
+        path = []
+        source = sized_node_list[0]
+        g = graph_type()
+        g[sized_node_list[0]] = None
+        path.append((sized_node_list[0], None))
+        for i in range(1, len(sized_node_list)):
+            path.append((sized_node_list[i], sized_node_list[i-1]))
+            g[sized_node_list[i]] = None
+            g.add_edge(sized_node_list[i-1], sized_node_list[i])
+
+        result = []
+        for i, node in enumerate(algorithm.search(g, source)):
+            assert node[0] == path[i][0]
+            assert node[1] == path[i][1]
+            result.append(node)
+
+        assert len(result) == len(path)
