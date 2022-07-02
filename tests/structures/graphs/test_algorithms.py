d32975d4b47e795ba40bd73ee29f6cbfb2be041b
Frank Di Natale <frank.dinatale1988@gmail.com>
2022-07-02 15:50:46 -0700

    Tweaks to DFS.

--- tests/structures/graphs/test_algorithms.py
+++ tests/structures/graphs/test_algorithms.py
@@ -1,3 +1,4 @@
+from math import floor, log2
 import pytest
 from typing import List, Type
 
@@ -16,27 +17,40 @@ from pyaestro.structures.graphs import (
 GRAPHS = (AcyclicAdjGraph, AdjacencyGraph, BidirectionalAdjGraph)
 
 
-@pytest.mark.parametrize("algorithm", (BreadthFirstSearch, DepthFirstSearch))
 @pytest.mark.parametrize("graph_type", GRAPHS)
-class TestGraphAlgorithms:
+class TestDepthFirstSearch:
     def test_tree_search(
-        self, sized_node_list: List[str], algorithm: GraphSearchProtocol,
-        graph_type: Type[Graph]
+        self, sized_node_list: List[str], graph_type: Type[Graph]
     ) -> None:
         """Tests path searching on a tree-structured graph.
 
         Args:
             sized_node_list (List[str]): A list of unique node names.
-            algorithm (GraphSearchProtocol): Search algorithm run testing on.
             graph_type (Type[Graph]): A Graph class name to test.
         """
         g = graph_type()
-        for node in sized_node_list:
-            g[node] = None
+        g[sized_node_list[0]] = 0
+
+        for i in range(1, len(sized_node_list)):
+            g[sized_node_list[i]] = floor(log2(i + 1))
+            parent = int((i - 1) / 2)
+            g.add_edge(sized_node_list[parent], sized_node_list[i])
+
+        path = list(DepthFirstSearch.search(g, sized_node_list[0]))
+        # In this case, we expect to hit all nodes; lengths must match since
+        # all nodes are in path.
+        assert len(path) == len(sized_node_list)
+        # There is no parent to the root node on the path.
+        assert path[0][1] == None
+        for node, parent in path[1::]:
+            # We expect that the current node in the path to be +1 depth from
+            # its parent.
+            print(path)
+            print(g._vertices)
+            assert g[node] == (g[parent] + 1)
 
     def test_linear_search(
-        self, sized_node_list: List[str], algorithm: GraphSearchProtocol,
-        graph_type: Type[Graph]
+        self, sized_node_list: List[str], graph_type: Type[Graph]
     ) -> None:
         """Tests path searching on a linear chain graph.
 
@@ -46,7 +60,6 @@ class TestGraphAlgorithms:
 
         Args:
             sized_node_list (List[str]): A list of unique node names.
-            algorithm (GraphSearchProtocol): Search algorithm run testing on.
             graph_type (Type[Graph]): A Graph class name to test.
         """
         print(sized_node_list)
@@ -61,7 +74,7 @@ class TestGraphAlgorithms:
             g.add_edge(sized_node_list[i-1], sized_node_list[i])
 
         result = []
-        for i, node in enumerate(algorithm.search(g, source)):
+        for i, node in enumerate(DepthFirstSearch.search(g, source)):
             assert node[0] == path[i][0]
             assert node[1] == path[i][1]
             result.append(node)
